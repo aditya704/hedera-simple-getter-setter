cefbf63e4227221bfbf7846ab19762ac4c927fb8
aditya704 <60214953+aditya704@users.noreply.github.com>
2022-06-19 15:07:16 +0530

    Update LookupContract.sol

--- LookupContract.sol
+++ LookupContract.sol
@@ -3,19 +3,19 @@ pragma solidity >=0.7.0 < 0.9.0;
 
 contract LookupContract {
 
-    mapping (string => uint) public myDirectory;
+    mapping (string => uint) public myDirectory;        // Defining the Mapping for directory
     
-    constructor(string memory _name, uint _mobileNumber) {
+    constructor(string memory _name, uint _mobileNumber) {      // Defining the constructor to be able to add new entries in the directory
         myDirectory[_name] = _mobileNumber;
 
     }
 
-    function setMobileNumber(string memory _name, uint _mobileNumber) public{
+    function setMobileNumber(string memory _name, uint _mobileNumber) public{       // Function for setting up a new entry
 
         myDirectory[_name] = _mobileNumber;
     }
 
-    function getMobileNumber(string memory _name) public view returns(uint){
+    function getMobileNumber(string memory _name) public view returns(uint){        // Function to get data about already existing entry
 
         return myDirectory[_name];
     }
